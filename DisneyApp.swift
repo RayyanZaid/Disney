254cd91d84e36954fb545ce9ab5ae1eccb91beaa
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-07-17 16:09:43 -0700

    first commit

---
+++ DisneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyApp.swift
+//  Disney
+//
+//  Created by Rayyan Zaid on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DisneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
